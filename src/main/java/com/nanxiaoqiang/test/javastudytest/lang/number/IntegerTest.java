1a598ee2a21fbe9a6b6a8b3d6b3b4c175799ece2
南肖墙 <nanxiaoqiang@163.com>
2014-07-04 22:49:39 +0800

    Integer和int的比较。

--- src/main/java/com/nanxiaoqiang/test/javastudytest/lang/number/IntegerTest.java
+++ src/main/java/com/nanxiaoqiang/test/javastudytest/lang/number/IntegerTest.java
@@ -18,6 +18,15 @@ public class IntegerTest {
 		// 输出最大最小值
 		System.out.println(Integer.MAX_VALUE);
 		System.out.println(Long.MAX_VALUE);
+		int num1 = 128;
+		int num2 = 128;
+		Integer num3 = 128;
+		Integer num4 = 128;
+		System.out.println(num1 == num2);
+		System.out.println(num3 == num4);// 注意！两个Integer的比较仍然是比较指针的内存值（栈）。所以会不一样！
+		System.out.println(num3.intValue() == num4);
+		System.out.println(num3.intValue() == num4.intValue());
+		System.out.println(num1 == num3);
 	}
 
 }
