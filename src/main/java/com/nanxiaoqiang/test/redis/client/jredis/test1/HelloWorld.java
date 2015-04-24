d106465ade6a072ec0dfa65df7080e30734aaffe
nanxiaoqiang <nanxiaoqiang@163.com>
2015-04-24 17:28:26 +0800

    加了个MGET读取多个结果集

--- src/main/java/com/nanxiaoqiang/test/redis/client/jredis/test1/HelloWorld.java
+++ src/main/java/com/nanxiaoqiang/test/redis/client/jredis/test1/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.nanxiaoqiang.test.redis.client.jredis.test1;
 
+import java.util.List;
+
 import redis.clients.jedis.Jedis;
 
 public class HelloWorld {
@@ -21,6 +23,11 @@ public class HelloWorld {
 		System.out.println(jedis.del(key));// 删除成功返回1
 		System.out.println(jedis.select(0));// 此时再选择db0
 		System.out.println(jedis.get(key));// 此时输出的是db0的key的值
+		System.out.println("***********");
+		List<String> list = jedis.mget("null", key, "no");
+		for (String string : list) {
+			System.out.println(string);
+		}
 		jedis.disconnect();
 		jedis.close();
 	}
