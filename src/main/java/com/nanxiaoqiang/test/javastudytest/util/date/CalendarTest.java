2477a0ceac6cab7f3433610b423cf71d0106f1f7
nanxiaoqiang <nanxiaoqiang@163.com>
2015-09-03 13:19:17 +0800

    Calendar的测试，从long得到时间。get(Calendar.MONTH)是从0开始计算的，但是奇怪DATE是从1开始的。

--- src/main/java/com/nanxiaoqiang/test/javastudytest/util/date/CalendarTest.java
+++ src/main/java/com/nanxiaoqiang/test/javastudytest/util/date/CalendarTest.java
@@ -1,7 +1,9 @@
 package com.nanxiaoqiang.test.javastudytest.util.date;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import org.joda.time.DateTime;
 
@@ -24,5 +26,21 @@ public class CalendarTest {
 		System.out.println(dt);
 		System.out.println(System.currentTimeMillis());
 		// Long timea = System.currentTimeMillis();
+		System.out.println("***************");
+		long time = System.currentTimeMillis();
+		try {
+			TimeUnit.SECONDS.sleep(2);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		long time2 = System.currentTimeMillis();
+		Calendar c1 = Calendar.getInstance();
+		c1.setTimeInMillis(time);
+		Date d2 = new Date(time2);
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+		System.out.println(sdf.format(c1.getTime()));
+		System.out.println(sdf.format(d2));
+		System.out.println(c1.get(Calendar.YEAR) + "-" + c1.get(Calendar.MONTH)// 只有月份是从0开始的
+				+ "-" + c1.get(Calendar.DATE));
 	}
 }
