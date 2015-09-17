d5e9fd5fd145215afa63cf2aa831c79bed5234da
nanxiaoqiang <nanxioqiang@163.com>
2015-09-17 14:11:59 +0800

    new BigDecimal(9.6).divide(new BigDecimal(3.0), RoundingMode.HALF_UP)还是会出现double/double除不尽的结果。 但是如果new BigDecimal("9.6")去除就没有问题

---
+++ src/main/java/com/nanxiaoqiang/test/javastudytest/lang/math/BigIntegerTest.java
@@ -0,0 +1,25 @@
+package com.nanxiaoqiang.test.javastudytest.lang.math;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class BigIntegerTest {
+
+	public static void main(String[] args) {
+		double d1 = 9.6;
+		double d2 = 3.0;
+		double d3 = 3.2;
+		System.out.println(d1 / d2);
+		System.out.println(d1 / d3);
+		BigDecimal b1 = new BigDecimal(9.6);
+		BigDecimal b2 = new BigDecimal(3.0);
+		BigDecimal b3 = b1.divide(b2, RoundingMode.HALF_UP);
+		BigDecimal b4 = b1.divide(b2, 8, RoundingMode.HALF_UP);
+		BigDecimal b5 = new BigDecimal("9.6");
+		System.out.println(b3 + "|" + b4);// 直接除还是不对
+		System.out.println(new BigDecimal(9.6).divide(new BigDecimal(3.0),
+				RoundingMode.HALF_UP));
+		System.out.println(b5.divide(b2));// String 转换后的除法正常
+	}
+
+}
