10e1a1a6885eeed700c2cba6cd0791829bcbadf6
南肖墙 <nanxiaoqiang@163.com>
2015-09-18 14:39:03 +0800

    java double 本身就不是一个准确值，输出BigDecimal double 9.6就可以看出来（感谢豆瓣 @头头带你飞）

--- src/main/java/com/nanxiaoqiang/test/javastudytest/lang/math/BigIntegerTest.java
+++ src/main/java/com/nanxiaoqiang/test/javastudytest/lang/math/BigIntegerTest.java
@@ -12,10 +12,13 @@ public class BigIntegerTest {
 		System.out.println(d1 / d2);
 		System.out.println(d1 / d3);
 		BigDecimal b1 = new BigDecimal(9.6);
+		// java double 本身就不是一个准确值，输出BigDecimal double 9.6就可以看出来（感谢豆瓣 @头头带你飞）
+		System.out.println("BigDecimal double 9.6:" + b1.toString());
 		BigDecimal b2 = new BigDecimal(3.0);
 		BigDecimal b3 = b1.divide(b2, RoundingMode.HALF_UP);
 		BigDecimal b4 = b1.divide(b2, 8, RoundingMode.HALF_UP);
 		BigDecimal b5 = new BigDecimal("9.6");
+		System.out.println("BigDecimal String 9.6:" + b5.toString());
 		System.out.println(b3 + "|" + b4);// 直接除还是不对
 		System.out.println(new BigDecimal(9.6).divide(new BigDecimal(3.0),
 				RoundingMode.HALF_UP));
